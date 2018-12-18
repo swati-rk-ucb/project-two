1250a9d34bcc33b4d248c6ecf2d488aff22f4dda
swati-rk-ucb <43649579+swati-rk-ucb@users.noreply.github.com>
2018-12-17 22:43:58 -0800

    Add files via upload

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import requests
 import json
 from process_response import trim_response
 import pprint
+from bson.json_util import dumps
 
 # Flask Setup
 app = Flask(__name__)
@@ -15,53 +16,49 @@ conn = 'mongodb://localhost:27017'
 client = pymongo.MongoClient(conn)
 db = client.raw_data
 collection = db.all_month
-collection.drop()
 processed_collection = db.custom_response
+filter_response = db.filter_response
+
+
+#home route
 @app.route("/")
 def index():
 	response_data = None
 	try:
 		response = requests.get("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson").json()
+		#collection.drop()
 		collection.insert_one(response)
-		response_data = trim_response(response)
-		db.custom_response.insert_one(trim_response(response))
+		filter_response.drop()
+		response_data = trim_response(response, 1)
+		processed_collection.drop()
+		processed_collection.insert_one(trim_response(response, 0))
 	except Exception as e:
 		print(e)
 
 	return jsonify(response_data)
 
-@app.route("/geo-mapping")
-def mapping():
-	try:
-		#geolocator = Nominatim(user_agent="ucb-da")
-		response = requests.get("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson").json()
-		#print(response)
-		features = response["features"]
-		for feature in features:
-			#print(feature)
-			location = feature["geometry"]["coordinates"]
-			#g = geocoder.google([location[1], location[2]], method='reverse')
-			#print(g)
-			#locationDetails = geolocator.reverse(location[1], location[0])
-			#print(locationDetails)
-			#print(location[1])
-			mapurl = 'https://maps.googleapis.com/maps/api/geocode/json?latlng={},{}&key=YOUR_KEY'.format(location[1], location[0])
-			#print(mapurl)
-			mapres = requests.get(mapurl).json()
-			#print(mapres)
-			#components = mapres['results'][0]['address_components']
-			#for c in components:
-				#if "country" in c['types']:
-					#country = c['long_name']
-					#print(country)
-				#if "postal_town" in c['types']:
-					#town = c['long_name']
-					#print(town)
-		#latest_data = db.allcountries
-	except Exception as e:
-		print(e)
-
-	return ""
+@app.route("/filter/<date>")
+@app.route("/filter/<date>/<region>")
+@app.route("/filter/<date>/<region>/<tw>")
+@app.route("/filter/<date>/<region>/<tw>/<mag>")
+def user_filter(date=None, region=None, tw=None, mag=None):
+	user_fi = {}
+	if (date is not None and date is not 0):
+		user_fi['formatted_only_date'] = str(date)
+	if (region is not None and region is not 'any'):
+		user_fi['region'] = str(region)
+	if (tw is not None):
+		user_fi['tsunami'] = int(tw)
+	
+	if (user_fi is not None):
+		res = filter_response.find(user_fi)
+		print(filter_response.count_documents(user_fi))
+	else:
+		res = filter_response.find()
+	
+	#data = filter_response.find({"formatted_only_date" : "2018-12-18"})
+	#print(filter_response.count_documents({"formatted_only_date" : "2018-12-18"}))
+	return dumps(res)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- process_response.py
+++ process_response.py
@@ -1,42 +1,42 @@
 import time
 from datetime import datetime
+import pymongo
 
-def trim_response(res):
+south_america = ['Argentina', 'Bolivia', 'Brazil','Colombia', 'Ecuador', 'Peru', 'Chile','Guyana', 'Paraguay', 'Suriname', 'Uruguay', 'Venezuela', 'French Guiana', 'Puerto Rico', 'Papua New Guinea', 'U.S. Virgin Islands', 'Dominican Republic', 'British Virgin Islands']
+asia = ['Indonesia', 'India', 'Pakistan', 'Afghanistan', 'Japan', 'Japan Region', 'Russia', 'offshore Azerbaijan', 'Azerbaijan', 'Philippines', 'Burma', 'Myanmar', 'China', 'Taiwan','Kyrgyzstan', 'East Timor', 'Northern Mariana Islands', 'Micronesia', 'Solomon Islands']
+north_america = ['CA', 'Alaska', 'Hawaii', 'Nevada', 'California', 'Montana', 'Washington', 'United States', 'Oklahoma', 'Tennessee', 'Utah', 'Canada', 'Kansas', 'Nicaragua', 'Missouri', 'Mexico', 'MX', 'Wyoming', 'Oregon', 'Idaho', 'Colorado', 'Arizona']
+anz = ['Fiji', 'Australia', 'Vanuatu', 'New Caledonia', 'New Zealand', 'Tonga']
+
+conn = 'mongodb://localhost:27017'
+# Pass connection to the pymongo instance.
+client = pymongo.MongoClient(conn)
+db = client.raw_data
+filter_response = db.filter_response
+
+def trim_response(res, flag):
     all_events = []
     tsunami_triggered_count = 0
     tsunami_not_triggered_count = 0
     timeseries_data = {}
     if (res is not None):
         for feature in res["features"]:
-            tsunami_event = feature["properties"]["tsunami"]
-            ts = feature["properties"]["time"]
-            formatted_date = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(ts/1000))
-            only_date = time.strftime('%Y-%m-%d', time.gmtime(ts/1000))
-            #print(ts)
-            #print(time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(ts/1000)))
-            event = {
-                "date" : feature["properties"]["time"],
-                "formatted_date" : formatted_date,
-                "formattes_only_date" : only_date,
-                "location" : feature["geometry"]["coordinates"],
-                "magnitude" : feature["properties"]["mag"],
-                "significance" : feature["properties"]["sig"],
-                "tsunami" : tsunami_event,
-                "place" : feature["properties"]["place"]
-            }
+            event = generate_event(feature)
 
-            if (tsunami_event == 1):
+            if (event["tsunami"] == 1):
                 tsunami_triggered_count = tsunami_triggered_count + 1
             else:
                 tsunami_not_triggered_count = tsunami_not_triggered_count + 1
             
             # prepare dictionary for date and number of earthquakes
-            if only_date  in timeseries_data:
-                count = timeseries_data[only_date]
-                timeseries_data[only_date] = count+1
+            dt = event["formatted_only_date"]
+            if dt  in timeseries_data:
+                count = timeseries_data[dt]
+                timeseries_data[dt] = count+1
             else:
-                timeseries_data[only_date] = 1
+                timeseries_data[dt] = 1
             all_events.append(event)
+            if (flag == 1):
+                save_event(generate_event(feature))
     else:
         print("Response is empty") 
     
@@ -49,4 +49,51 @@ def trim_response(res):
         },
         "timeseries_data" : timeseries_data
     }
+    #print(complete_data)
     return complete_data
+
+def get_region(place):
+    tokens = place.split(',')
+    if (len(tokens) > 1):
+        location = tokens[len(tokens)-1].strip()
+    else:
+        location = tokens[0].strip()
+
+    return find_region_for_entity(location)
+
+def find_region_for_entity(location):
+    if location in north_america:
+        return "North America"
+    elif location in south_america:
+        return "South America"
+    elif location in asia:
+        return "Asia"
+    elif location in anz:
+        return "ANZ"
+    else:
+        return "Other"
+
+def save_event(e):
+    filter_response.insert_one(e)
+
+def generate_event(data):
+    tsunami_event = data["properties"]["tsunami"]
+    ts = data["properties"]["time"]
+    formatted_date = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(ts/1000))
+    only_date = time.strftime('%Y-%m-%d', time.gmtime(ts/1000))
+    geo_location = data["properties"]["place"]
+    #print(ts)
+    #print(time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(ts/1000)))
+    event_dict = {
+        "date" : data["properties"]["time"],
+        "formatted_date" : formatted_date,
+        "formatted_only_date" : only_date,
+        "location" : data["geometry"]["coordinates"],
+        "magnitude" : data["properties"]["mag"],
+        "significance" : data["properties"]["sig"],
+        "tsunami" : tsunami_event,
+        "place" : geo_location,
+        "region" : get_region(geo_location)
+    }
+
+    return event_dict
