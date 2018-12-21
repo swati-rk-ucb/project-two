cca0499813f024f72c58b3de39bc09e8126bbe79
Swati Patil <swati_aprk@yahoo.com>
2018-12-20 21:02:35 -0800

    merge changes

--- static/js/plots.js
+++ static/js/plots.js
@@ -1,6 +1,7 @@
 function init() {
   d3.json("/all").then((data) => {
     timeScatterPlot(data);
+    sigScatterPlot(data);
   });
 
 }
@@ -15,14 +16,20 @@ function filterCharts(id, selValue) {
   console.log(url);
   d3.json(url).then((data) => {
     timeScatterPlot(data);
+    sigScatterPlot(data);
   });
 }
 
+var div_colors = {"mag7" : "#ffff99", 
+  "mag6" : "#ffcc66", "mag5" : "#ffaa00", "mag4" : "#ff751a", 
+  "mag3" : "#cc0000", "mag2" : "#7a0000", "mag1" : "#3d0000"
+};
+
 function filter_data(id, value) {
-  console.log(id);
-  var col = d3.select("." + id);
-  console.log(col.attr);
-  col.attr("class", "magnitude-col-sel");
+  console.log(id, div_colors[id]);
+  var col = d3.select("#" + id);
+  col.style("background-color", '"'+div_colors[id]+'"');
+  clearOtherCells(id);
   var date_filter = d3.select("#date-select").node().value;
   var region_filter = d3.select("#region-select").node().value;
   var tsunami_filter = d3.select("#tw-select").node().value;
@@ -32,7 +39,18 @@ function filter_data(id, value) {
   console.log(url);
   d3.json(url).then((data) => {
     timeScatterPlot(data);
+    sigScatterPlot(data);
   });
+}
+
+function clearOtherCells(id) {
+  var mag_cols = ["mag1", "mag2", "mag3", "mag4", "mag5", "mag6","mag7"];
+  for (var i = 0; i < mag_cols.length; i++) {
+    if (id != mag_cols[i]) {
+      console.log(mag_cols[i]);
+      var col = d3.select("#" + id).style("background-color", "#FFFFFF");
+    }
+  }
 }
 
 function timeScatterPlot(data) {
@@ -71,5 +89,48 @@ function timeScatterPlot(data) {
     };
 
     Plotly.newPlot("plot", data, layout);
+}
+
+function sigScatterPlot(data) {
+  console.log("Number of records ", data.count);
+  var sig_value = d3.keys(data.sig_data);
+    var counts = d3.values(data.sig_data);
+
+    var sizes = [];
+    for (var i = 0; i < counts.length; i++) {
+      sizes[i] = counts[i];
+    }
+    var trace1 = {
+      type: "scatter",
+      mode: "markers",
+      opacity: 0.65,
+      x: sig_value,
+      y: counts,
+      marker: {
+        color: "red",
+        line:{
+          color: "black",
+          width: 0.5
+        }
+      }
+    };
+
+    var data = [trace1];
+
+    var layout = {
+      //title: `Number of Earthquakes per Day`,
+      xaxis: {
+        autorange: true,
+        type: "linear"
+      },
+      yaxis: {
+        autorange: true,
+        type: "linear"
+      },
+      height: 300,
+      width: 525
+    };
+
+    Plotly.newPlot("plot-2", data, layout);
 }
 init();
